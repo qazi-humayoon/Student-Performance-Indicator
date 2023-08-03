f8b602ac162f7abb2778529041609016753a40cf
qazi-humayoon <qazi.humayoon687@gmail.com>
2023-08-03 16:49:20 +0530

    Data Transformation Done

--- src/exception.py
+++ src/exception.py
@@ -3,7 +3,7 @@ from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
+    file_name=exc_tb.tb_frame.f_code.co_filename #refer to the documentation of custom exception
     error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
      file_name,exc_tb.tb_lineno,str(error))
 
